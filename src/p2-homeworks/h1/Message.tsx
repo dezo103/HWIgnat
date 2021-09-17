a1acea49a23af02c67e0c8669b16f318f3193cc3
Aliaksandr Rudyk <dezo103@gmail.com>
2021-09-17 15:28:13 +0300

    feat: completing first homework

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+
+}
+
+function Message(props: MessageType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.leftColumn}>
+                <img src={props.avatar} alt="ava"/>
+            </div>
+            <div className={s.rightColumn}>
+                <div className={s.messageArea}>
+                    <div className={s.firstName}>
+                        {props.name}
+                    </div>
+                    <div className={s.messageData}>
+                        <div className={s.messageText}>
+                            {props.message}
+                        </div>
+                        <div className={s.messageTime}>
+                            {props.time}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
